960da7e4f53860b6ac93ac5d713b83f91bd8e1bd
WeslliVs <wesllivs@gmail.com>
2021-11-13 20:41:23 -0300

    Atividade 06

---
+++ atividades/atividade06/analise_sintatica.py
@@ -0,0 +1,29 @@
+def expr():
+    term()
+    while (nextToken == ADD_OP | nextToken == SUB_OP):
+        lex()
+        term()
+    return expr
+
+
+def term():
+    print("Enter <term>: ")
+    factor()
+    while (nextToken == MULT_OP | nextToken == DIV_OP):
+        lex()
+        factor()
+    print("Exit <term> \n")
+    return term
+
+
+def factor():
+    if (nextToken == ID_CODE | nextToken == INT_CODE):
+        lex()
+    elif (nextToken == LP_CODE):
+        lex()
+        expr()
+        if (nextToken == RP_CODE):
+            lex()
+        else:
+            error()
+    return factor
